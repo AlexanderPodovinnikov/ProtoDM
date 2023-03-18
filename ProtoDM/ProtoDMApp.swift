34e86e007c6e5c7361515c917ba3aacd2fa2fddf
AlexanderPodovinnikov <art.iq@hotmail.com>
2023-03-18 14:26:28 +0200

    Initial Commit

---
+++ ProtoDM/ProtoDMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtoDMApp.swift
+//  ProtoDM
+//
+//  Created by Alex Po on 18.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProtoDMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
