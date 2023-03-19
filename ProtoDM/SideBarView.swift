e9edc81e14f7f1ea8b7045d1ce90efba6aaaec03
AlexanderPodovinnikov <art.iq@hotmail.com>
2023-03-19 20:49:11 +0200

    coredata helpers

--- ProtoDM/SideBarView.swift
+++ ProtoDM/SideBarView.swift
@@ -12,9 +12,9 @@ struct SideBarView: View {
     let smartFilters: [Filter] = [.all, .recent]
 
     @FetchRequest(sortDescriptors: [SortDescriptor(\.name)]) var tags: FetchedResults<Tag>
-    var tagFilters: [Filter] {
+    var tagFilters: [Filter] { //didChange ?
         tags.map {tag in
-            Filter(id: tag.id ?? UUID(), name: tag.name ?? "No name", icon: "tag", tag: tag)
+            Filter(id: tag.tagID, name: tag.tagName, icon: "tag", tag: tag)
         }
     }
 
@@ -31,6 +31,7 @@ struct SideBarView: View {
                 ForEach(tagFilters) { filter in
                     NavigationLink(value: filter) {
                         Label(filter.name, systemImage: filter.icon)
+                            .badge(filter.tag?.tagActiveIssues.count ?? 0)
                     }
                 }
             }
